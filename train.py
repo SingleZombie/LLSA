419494bbfc178ef7e4af6b73623fe5ab7053e625
SingleZombie <singlezombie@163.com>
2025-12-18 17:52:10 +0800

    update dependencies

--- train.py
+++ train.py
@@ -114,20 +114,20 @@ def main():
 
     # Initialize the model
     enable_xformers = False
-    if cfg.enable_xformers_memory_efficient_attention:
-        if is_xformers_available():
-            import xformers
+    # if cfg.enable_xformers_memory_efficient_attention:
+    #     if is_xformers_available():
+    #         import xformers
 
-            xformers_version = version.parse(xformers.__version__)
-            if xformers_version == version.parse("0.0.16"):
-                logger.warning(
-                    "xFormers 0.0.16 cannot be used for training in some GPUs. If you observe problems during training, please update xFormers to at least 0.0.17. See https://huggingface.co/docs/diffusers/main/en/optimization/xformers for more details."
-                )
-            enable_xformers = True
+    #         xformers_version = version.parse(xformers.__version__)
+    #         if xformers_version == version.parse("0.0.16"):
+    #             logger.warning(
+    #                 "xFormers 0.0.16 cannot be used for training in some GPUs. If you observe problems during training, please update xFormers to at least 0.0.17. See https://huggingface.co/docs/diffusers/main/en/optimization/xformers for more details."
+    #             )
+    #         enable_xformers = True
 
-        else:
-            raise ValueError(
-                "xformers is not available. Make sure it is installed correctly")
+    #     else:
+    #         raise ValueError(
+    #             "xformers is not available. Make sure it is installed correctly")
 
     trainer.init_modules(enable_xformers, cfg.gradient_checkpointing)
 
